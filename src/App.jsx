ea6c5b9f925c4f7a8fb66457ff7ed6590a5063a9
Shreyas <sonureigns007@gmail.com>
2022-11-14 18:42:02 +0530

    react modal component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Global.css";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
